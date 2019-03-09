f26c9acff6ff9a1d784bfa73239fe532e89b3ed2
Zachary Hughes <zachary.hughes@bright-wolf.com>
2019-03-09 02:53:31 -0500

    added error log stream, more readme information

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Hapi.js Boilerplate
 
 This repository is heavily influenced by [Hapi Starter Kit](https://github.com/Codigami/hapi-starter-kit)
+
+### Directory Structure
+
+```txt
+.
+|-- README.md
+|-- config # environment and configuration variables get put here
+|   `-- default.json
+|-- index.js
+|-- package.json
+|-- plugins.js
+|-- public # frontend public assets get put in this directory
+|-- routes.js
+|-- server
+|   `-- utils
+|       `-- logger.js
+|-- server.js
+
+```
--- server/utils/logger.js
+++ server/utils/logger.js
@@ -6,7 +6,17 @@ const config = require('config');
 const log = bunyan.createLogger({
     name: config.get('app.name'),
     level: config.get('app.logLevel'),
-    serializers: bunyan.stdSerializers
+    serializers: bunyan.stdSerializers,
+    streams: [
+        {
+          level: 'info',
+          stream: process.stdout            // log INFO and above to stdout
+        },
+        {
+          level: 'error',
+          path: `/var/tmp/${config.get('app.name')}-error.log`  // log ERROR and above to a file
+        }
+      ]
 });
 
 module.exports = log;
